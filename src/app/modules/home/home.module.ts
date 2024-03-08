c6de3824b079e03edbb45c8a2b7ccfa636ddcac1
Vadim Voitenko <mirvoy8031@gmail.com>
2024-03-08 22:57:42 +0200

    Add Header Component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,12 +6,10 @@ import { CoreModule } from 'src/app/core/core.module'
 import { LayoutModule } from 'src/app/shared/modules/layout/layout.module'
 
 import { HomeRoutingModule } from 'src/app/modules/home/home-routing.module'
-import { HomeHeroComponent } from 'src/app/modules/home/pages/home/home-hero/home-hero.component'
-import { HomeProjectsComponent } from 'src/app/modules/home/pages/home/home-projects/home-projects.component'
 import { HomeComponent } from 'src/app/modules/home/pages/home/home.component'
 
 @NgModule({
-  declarations: [HomeComponent, HomeHeroComponent, HomeProjectsComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, HomeRoutingModule, CoreModule, LayoutModule],
 })
 export class HomeModule {}
--- src/app/modules/home/pages/home/home-hero/home-hero.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-home-hero',
-  templateUrl: './home-hero.component.html',
-  styleUrls: ['./home-hero.component.scss'],
-})
-export class HomeHeroComponent {}
