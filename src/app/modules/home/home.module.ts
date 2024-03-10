66a5351e6952df9ed79366e79169fc3583287835
Vadim Voitenko <mirvoy8031@gmail.com>
2024-03-10 23:07:35 +0200

    Add Contact Component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -8,10 +8,11 @@ import { LayoutModule } from 'src/app/shared/modules/layout/layout.module'
 import { HomeRoutingModule } from 'src/app/modules/home/home-routing.module'
 import { HomeComponent } from 'src/app/modules/home/pages/home/home.component'
 import { ScheduleComponent } from './pages/home/schedule/schedule.component'
-import { MediaComponent } from './pages/home/media/media.component'
+import { MediaComponent } from './pages/home/media/media.component';
+import { ContactComponent } from './pages/home/contact/contact.component'
 
 @NgModule({
-  declarations: [HomeComponent, ScheduleComponent, MediaComponent],
+  declarations: [HomeComponent, ScheduleComponent, MediaComponent, ContactComponent],
   imports: [CommonModule, HomeRoutingModule, CoreModule, LayoutModule],
 })
 export class HomeModule {}
